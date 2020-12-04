9a2126e35d8dd5849206da98d041adebd957decf
Thorsten Smit <smit@bbs2leer.de>
2020-12-04 15:40:45 +0100

    Main

--- src/abi2020ifvorbereitung/ABI2020IFVorbereitung.java
+++ src/abi2020ifvorbereitung/ABI2020IFVorbereitung.java
@@ -3,9 +3,22 @@ package abi2020ifvorbereitung;
 public class ABI2020IFVorbereitung {
 
     public static void main(String[] args) {
+
+        //Strukturierte Programmierung
+        double zahl1 = 5.45;
+        double zahl2 = 6.789;
+        
+        while(zahl1 > 0){
+            zahl1 = zahl1 - 0.9;
+            if(zahl1 < 1.0){
+                zahl1 += 0.1;    //Kurzdeklaration von zahl1 = zahl1 + 0.1; 
+            }
+            System.out.println(zahl1);
+        }
+        
         
         
         
     }
-    
+
 }
