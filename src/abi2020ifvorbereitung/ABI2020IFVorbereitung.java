669683c8974e507eddfc5ae723a04cbeeeacd747
Thorsten Smit <smit@bbs2leer.de>
2020-12-04 12:19:13 +0100

    Test

---
+++ src/abi2020ifvorbereitung/ABI2020IFVorbereitung.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abi2020ifvorbereitung;
+
+/**
+ *
+ * @author Anwender
+ */
+public class ABI2020IFVorbereitung {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
