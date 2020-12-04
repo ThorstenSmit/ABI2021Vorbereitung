73dc737c0b08139d709f193659fc2530dfcff276
Thorsten Smit <smit@bbs2leer.de>
2020-12-04 16:14:20 +0100

    Main

--- src/abi2020ifvorbereitung/ABI2020IFVorbereitung.java
+++ src/abi2020ifvorbereitung/ABI2020IFVorbereitung.java
@@ -1,5 +1,8 @@
 package abi2020ifvorbereitung;
 
+import grundlagenjava.Grundlagen;
+import static grundlagenjava.Grundlagen.*;
+
 public class ABI2020IFVorbereitung {
 
     public static void main(String[] args) {
@@ -16,9 +19,21 @@ public class ABI2020IFVorbereitung {
             System.out.println(zahl1);
         }
         
+        //import, da nicht im selben package/namespace
+        int unterrichtszeitHeute = MAX_TIME_TODAY;
         
+        //Beispiel: Nutzung von Konstanten als Array-Zugriff
+        System.out.println("MWST: "+Grundlagen.steuersaetze[Grundlagen.MWST_REDUZIERT_REGEL]+"%");
         
-        
+        //Beispiel: typeCast - implizit ::: KEINE VERLUST!!
+        int ganzzahl = 3;
+        double decZahl = ganzzahl;
+        //Beispiel: typeCast - explizit ::: VERLUST!!
+        //float floatZahl = decZahl; 
+        //http://java.tsmit.de/explizite_typwandlung.html
+        //http://java.tsmit.de/datentypen.html
+        float floatZahl = (float) decZahl;
+        System.out.println("Typecasted Value: "+floatZahl);
     }
 
 }
